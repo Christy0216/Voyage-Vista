2a19ff567e226bf46c667957879737fe2741e4d3
kai-zong <zong.k@northeastern.edu>
2024-08-05 13:35:44 -0700

    Update .gitigonre and babel config file, add firebase config
    Install dotenv; Create and add .env file to the gitignore; Add the firebase config to the env file

---
+++ Voyage-Vista/firebase/firebaseSetUp.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId} from '@env';
+
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
+}
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

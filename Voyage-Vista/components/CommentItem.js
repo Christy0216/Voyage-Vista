dbaee346f7c9550f46491bda43a0d42fcc7f979b
kai-zong <zong.k@northeastern.edu>
2024-08-05 17:09:11 -0700

    add some reusable component prototype
    including PostItem for each post in home page or other pages, CommentItem for each comment in postDetailScreen, and the InputField for submitting field values to the server

---
+++ Voyage-Vista/components/CommentItem.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function commentItem() {
+  return (
+    <View>
+      <Text>commentItem</Text>
+    </View>
+  )
+}

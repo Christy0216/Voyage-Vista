e96096e9835f80f574505730432c0579b0d27790
Shirui Chen <chen.shir@northeastern.edu>
2024-08-05 18:35:07 -0700

    implments post item components for reusage, reuse the components in main screen and favorites screen to avoid code duplication

--- Voyage-Vista/components/PostItem.js
+++ Voyage-Vista/components/PostItem.js
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function postItem() {
-  return (
-    <View>
-      <Text>postItem</Text>
-    </View>
-  )
-}
+const PostItem = ({ post }) => {
+    const navigation = useNavigation();
+
+    return (
+        <TouchableOpacity onPress={() => navigation.navigate('PostDetailsScreen', { postId: post.id })}>
+            <View>
+                <Image source={{ uri: post.profilePicture }} style={{ width: 100, height: 100 }} />
+                <Text>{post.title}</Text>
+                <View>
+                    {post.photos && post.photos.slice(0, 4).map((photo, index) => (
+                        <Image key={index} source={{ uri: photo.url }} style={{ width: 100, height: 100 }} />
+                    ))}
+                </View>
+                <Text>Favorites: {post.favoritesCount}</Text>
+                <Text>Likes: {post.likesCount}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+export default PostItem;
--- Voyage-Vista/screens/FavoritesScreen.js
+++ Voyage-Vista/screens/FavoritesScreen.js
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { db } from '../firebase/firebaseSetUp';
-import { doc, getDoc, getDocs } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
+import PostItem from '../components/PostItem';  // Ensure this path is correct
 import { useNavigation } from '@react-navigation/native';
 
 const FavoritesScreen = () => {
     const [favorites, setFavorites] = useState([]);
     const navigation = useNavigation();
-    const userId = 'your_user_id_here'; // This should be dynamically set based on the logged-in user
+    const userId = 'your_user_id_here';  // Dynamically set this based on logged-in user
 
     useEffect(() => {
         const fetchFavorites = async () => {
-            // Fetch the user's document to get the array of favorite post IDs
             const userDocRef = doc(db, 'users', userId);
             const userDoc = await getDoc(userDocRef);
             if (userDoc.exists() && userDoc.data().favorites) {
                 const favoritePostIds = userDoc.data().favorites;
                 const posts = [];
 
-                // Fetch each post by ID
                 for (const postId of favoritePostIds) {
                     const postRef = doc(db, 'posts', postId);
                     const postDoc = await getDoc(postRef);
@@ -26,7 +25,6 @@ const FavoritesScreen = () => {
                         posts.push({ id: postDoc.id, ...postDoc.data() });
                     }
                 }
-
                 setFavorites(posts);
             } else {
                 console.log('No favorites found or user does not exist');
@@ -41,18 +39,7 @@ const FavoritesScreen = () => {
             <FlatList
                 data={favorites}
                 keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('PostDetailsScreen', { postId: item.id })}>
-                        <View>
-                            <Text>{item.title}</Text>
-                            <View>
-                                {item.photos && item.photos.slice(0, 4).map((photo, index) => (
-                                    <Image key={index} source={{ uri: photo.url }} style={{ width: 100, height: 100 }} />
-                                ))}
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                )}
+                renderItem={({ item }) => <PostItem post={item} />}
             />
         </View>
     );

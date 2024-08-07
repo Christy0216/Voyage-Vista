b31bc90b905346557062f1d9daae345beacd6116
Shirui Chen <chen.shir@northeastern.edu>
2024-08-06 17:15:30 -0700

    implemented toggle screen functionality in setting screen

---
+++ Voyage-Vista/context/ThemeContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from 'react';
+import { themes } from '../styles/themes';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(themes.light);
+
+  const toggleTheme = () => {
+    setTheme(theme === themes.light ? themes.dark : themes.light);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);

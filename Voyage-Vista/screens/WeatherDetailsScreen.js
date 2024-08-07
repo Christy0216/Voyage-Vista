e2166aa6cc51c84a2a6b91a0356765211dc37708
kai-zong <zong.k@northeastern.edu>
2024-08-07 09:37:12 -0700

    feat: Edit WeatherDetailsScreen and CitySelectionModal components
    This commit edits the WeatherDetailsScreen component and adds the CitySelectionModal component. The WeatherDetailsScreen component displays the current weather details, three-hour weather summaries, and daily weather summaries. The CitySelectionModal component allows the user to select a city for weather information. These components are essential for providing weather-related functionality to the app.

--- Voyage-Vista/screens/WeatherDetailsScreen.js
+++ Voyage-Vista/screens/WeatherDetailsScreen.js
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import ThreeHourSummary from '../components/ThreeHourSummary';
+import DailySummary from '../components/DailySummary';
 
-export default function WeatherDetailsScreen() {
+const WeatherScreen = () => {
+  const threeHourSummaries = [
+    '3-Hour Summary 1',
+    '3-Hour Summary 2',
+    '3-Hour Summary 3',
+  ];
+
+  const dailySummaries = [
+    'Day 1 Summary',
+    'Day 2 Summary',
+    'Day 3 Summary',
+  ];
+
   return (
     <View>
-      <Text>WeatherDetailsScreen</Text>
+      {/* Top Part: Current Weather Details */}
+      <View>
+        <Text>Current Weather Details</Text>
+      </View>
+
+      {/* Middle Part: Horizontally Scrollable Weather Summaries */}
+      <FlatList
+        horizontal
+        data={threeHourSummaries}
+        renderItem={({ item }) => <ThreeHourSummary summary={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        showsHorizontalScrollIndicator={false}
+      />
+
+      {/* Bottom Part: Non-scrollable Weather Summaries for Following Days */}
+      <FlatList
+        data={dailySummaries}
+        renderItem={({ item }) => <DailySummary summary={item} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
-  )
-}
+  );
+};
+
+export default WeatherScreen;

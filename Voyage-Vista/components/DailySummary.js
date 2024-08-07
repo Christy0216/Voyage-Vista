c620ddfe9ecd6235fa7ac045a72b5b2b72faf6a4
kai-zong <zong.k@northeastern.edu>
2024-08-07 10:25:44 -0700

    feat: Connect OpenWeather Api and adjust weather components to receive data
    This commit edits the "ThreeHourSummary" and "DailySummary" components to receive data, and create a weatherManager to manage Api calls to the openWeather

--- Voyage-Vista/components/DailySummary.js
+++ Voyage-Vista/components/DailySummary.js
@@ -4,7 +4,9 @@ import { View, Text } from 'react-native';
 const DailySummary = ({ summary }) => {
   return (
     <View>
-      <Text>{summary}</Text>
+      <Text>{summary.dt_txt.split(' ')[0]}</Text>
+      <Text>Temperature: {summary.main.temp}°C</Text>
+      <Text>Condition: {summary.weather[0].description}</Text>
     </View>
   );
 };

2da821576bff27b6b8dbe5645710425e50cc45f6
Shirui Chen <chen.shir@northeastern.edu>
2024-08-20 19:43:29 -0700

    modified the weather preview styling in map screen

--- Voyage-Vista/screens/MapScreen.js
+++ Voyage-Vista/screens/MapScreen.js
@@ -285,7 +285,7 @@ const MapScreen = ({ navigation }) => {
 
       {/* Weather summary */}
       <TouchableOpacity onPress={handleWeatherSummaryPress}>
-        <View style={[styles.weatherSummaryContainer, {backgroundColor: theme.inputBackgroundColor}]}>
+        <View style={[styles.weatherSummaryContainer, {backgroundColor: theme.postItemBackgroundColor}]}>
           <Text style={[styles.weatherSummaryText, { color: theme.textColor }]}>
             Weather Summary for {cityName}
           </Text>
--- Voyage-Vista/styles/themes.js
+++ Voyage-Vista/styles/themes.js
@@ -6,7 +6,7 @@ export const themes = {
       buttonTextColor: "#fff", // White text on buttons
       inputBackgroundColor: "#fff", // White input fields for clarity
       placeholderTextColor: "#a9a9a9", // Dark gray for placeholders
-      postItemBackgroundColor: "#87CEEB", // Light blue post item background
+      postItemBackgroundColor: "#ffffff", // Bright white for container backgrounds in light theme
     },
     dark: {
       backgroundColor: "#2F4F4F", // Dark slate gray background
@@ -15,7 +15,7 @@ export const themes = {
       buttonTextColor: "#fff", // White text on buttons
       inputBackgroundColor: "#555", // Slightly lighter gray for inputs
       placeholderTextColor: "#dcdcdc", // Light gray for placeholders
-      postItemBackgroundColor: "#4682B4", // Steel blue post item background
+      postItemBackgroundColor: "#3c6e71", /// A slightly different shade for container backgrounds in dark theme
     },
   };
   
